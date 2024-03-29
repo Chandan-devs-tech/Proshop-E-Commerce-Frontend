9adfce598eef49fddb4e23cd9b14c53ccb64ace0
Chandan-devs-tech <chandanguptabsb@gmail.com>
2024-03-30 01:58:31 +0530

    Modify src/App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from './components/Home';
 import Navbar from './components/Navbar';
+import LogIn from './components/LogIn';
 
 function App() {
   return (
-    <>
+    <Router>
       <Navbar />
-      <Home />
-    </>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<LogIn />} />
+      </Routes>
+    </Router>
   );
 }
 
