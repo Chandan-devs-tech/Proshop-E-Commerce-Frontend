9c556b9af542162e8f95620a7687a6d7d5b5b559
Chandan-devs-tech <chandanguptabsb@gmail.com>
2024-04-02 10:13:02 +0530

    Added src/components/ShoppingCart.jsx

---
+++ src/components/ShoppingCart.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./ShoppingCart.css";
+
+const ShoppingCart = () => {
+  return (
+    <div className="shopping-cart">
+      <h3>Your cart is empty</h3>
+    </div>
+  );
+};
+
+export default ShoppingCart;
