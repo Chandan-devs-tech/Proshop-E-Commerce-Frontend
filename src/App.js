428b71eba9c771c9554f6f9b13343b3f2db33b0b
Chandan-devs-tech <chandanguptabsb@gmail.com>
2024-04-02 10:14:46 +0530

    Modify src/App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from './components/Home';
 import Navbar from './components/Navbar';
-import LogIn from './components/LogIn';
+import LogIn from './components/login-signup-password/LogIn';
+import ShoppingCart from './components/ShoppingCart';
+import SignUp from './components/login-signup-password/SignUp';
+import ForgotPassword from './components/login-signup-password/ForgotPassword';
 
 function App() {
   return (
@@ -11,6 +14,9 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<LogIn />} />
+        <Route path="/register" element={<SignUp />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/cart" element={<ShoppingCart />} />
       </Routes>
     </Router>
   );
